39d63be12b22b1660e2ccf489c237b9590f630c4
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-08-16 20:59:44 +0200

    add animation and audio to index.js

--- index.js
+++ index.js
@@ -2,8 +2,14 @@ let buttonColors = ["red", "blue", "green", "yellow"]
 let gamePatterns = []
 
 function nextSequence() {
+    //pick random color for the game
     let randomNumber = Math.floor(Math.random()*4)
     let randomChosenColor = buttonColors[randomNumber]
+    //assign to game pattern
     gamePatterns.push(randomChosenColor)
+    //add flash animation to the button
+    $(`#${randomChosenColor}`).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100)
+    //add audio to the button
+    let a = new Audio(`sounds/${randomChosenColor}.mp3`);
+    a.play()
 }
-
